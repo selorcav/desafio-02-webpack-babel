dfec57716c4bdeab1f121a504d6158b0d288b519
sebastian <sebastian.lorca@wic.uchile.cl>
2020-09-04 18:55:04 -0400

    resultado en three.js

--- js/one.js
+++ js/one.js
@@ -1,10 +1,12 @@
 const arry = [1, 4, 23, -4, 'one', 6, 0, 1.1, 3.1415]
+const result = []
 
 const multiplicar = ()=> {
     arry.forEach(element=>{
         let resultadoUno = (element*4)
         if (resultadoUno>8){
-            console.log(resultadoUno);
+            result.push(resultadoUno)
         }
     });
+    return result;
 }
--- js/two.js
+++ js/two.js
@@ -2,8 +2,7 @@ const objJson = {type: 'car', brand: 'Subaru', model: 'Legacy', year: '2003' };
 
 const myJson = ()=>{
     let stringJson = JSON.stringify(objJson);
-    console.log(stringJson);
-    
+    return stringJson;
 }
 
 
